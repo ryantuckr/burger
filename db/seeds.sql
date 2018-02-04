f35654fb5e2a542708eb6d448fe0356d5323703e
Ryan Tucker <ryantuckr@yahoo.com>
2018-02-04 15:54:50 -0500

    first push

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Greek Burger', false);
+
+
