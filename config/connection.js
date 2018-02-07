1fdf72b3d001fce645e774c3687dc1b5d0128dab
Ryan Tucker <ryantuckr@yahoo.com>
2018-02-06 22:04:29 -0500

    sadfsad

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,23 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-// if(process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// }else {
-//   connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'root',
-//     database: 'burgers_db'
-//   });
-// }
-var connection = mysql.createConnection({
-  port: 8889,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db'
+  });
+}
+// var connection = mysql.createConnection({
+//   port: 8889,
+//   host: "localhost",
+//   user: "root",
+//   password: "root",
+//   database: "burgers_db"
+// });
 
 // Make connection.
 connection.connect(function(err) {
